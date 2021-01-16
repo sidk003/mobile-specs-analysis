bf79eaba4cb4d45dda4eff3a04540987d4b828ff
sidk003 <siddhu1583@gmail.com>
2021-01-17 01:36:37 +0530

    MongoDB schema modified for table

--- server/models/Apple.js
+++ server/models/Apple.js
@@ -38,6 +38,73 @@ const AppleSchema = new mongoose.Schema({
       value: { type: Number, required: true },
     },
   },
+  table: {
+    phone1: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    phone2: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    phone3: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    phone4: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    phone5: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    phone6: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    phone7: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    phone8: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    phone9: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    phone10: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    phone11: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    phone12: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+    phone13: {
+      name: { type: String, required: true },
+      date: { type: String, required: true },
+      price: { type: Number, required: true },
+    },
+  },
 });
 
 module.exports = mongoose.model("Apple", AppleSchema);
